a9855e01b9130d810dddca6a8d06c5efb07e1a59
tivanovski <ivanovski.todor@gmail.com>
2017-07-08 23:42:14 +0300

    sd

--- views/site/index.php
+++ views/site/index.php
@@ -2,12 +2,12 @@
 
 /* @var $this yii\web\View */
 
-$this->title = 'My Yii Application';
+$this->title = 'Dental System';
 ?>
 <div class="site-index">
 
     <div class="jumbotron">
-        <h1>Congratulations!</h1>
+        <h1>Hello Worldddddd!</h1>
 
         <p class="lead">You have successfully created your Yii-powered application.</p>
 
