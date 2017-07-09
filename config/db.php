0dcfac276ac6eb9842cbf5defae49adbeda309a6
tivanovski <ivanovski.todor@gmail.com>
2017-07-09 16:13:37 +0300

    cx

--- config/db.php
+++ config/db.php
@@ -3,7 +3,7 @@ $dbopts = parse_url(getenv('DATABASE_URL'));
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'pgsql:host='.$dbopts["host"].':'.$dbopts["port"].';dbname='.ltrim($dbopts["path"],'/'),
+    'dsn' => 'pgsql:host='.$dbopts["host"].';dbname='.ltrim($dbopts["path"],'/'),
     'username' =>  $dbopts["user"],
     'password' =>  $dbopts["pass"],
     'charset' => 'utf8',
